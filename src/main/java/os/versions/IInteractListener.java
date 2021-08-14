d61b6d73a0c3b058a5867aca0ab950b51f975515
furkanaliunal <35196666+furkanaliunal@users.noreply.github.com>
2021-08-14 22:46:51 +0300

    Versions added

---
+++ src/main/java/os/versions/IInteractListener.java
@@ -0,0 +1,19 @@
+package os.versions;
+
+import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.ItemFrame;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
+
+public interface IInteractListener extends Listener {
+    public void hideItemFrameWhileSneaking(PlayerInteractAtEntityEvent e);
+    public void openContainerWhenFrameInvisible(PlayerInteractEntityEvent e);
+    public void rotateFrameItemWhileSneaking(EntityDamageByEntityEvent e);
+    public boolean openContainer(Player player, Block block);
+    public void rotateItemFrame(ItemFrame itemFrame);
+    boolean isItemFrame(EntityType item);
+}
